dc5978c24a2c1c4f4f11d2c735da116dfdc1f41e
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-04-06 17:17:54 +0400

    Update VPNWrapper.h

--- openvpn/VPNWrapper.h
+++ openvpn/VPNWrapper.h
@@ -1,19 +1,45 @@
-//
-//  VPNWrapper.h
-//  openvpn
-//
-//  Created by Christopher Ballinger on 11/3/13.
-//
-//
-
 #import <Foundation/Foundation.h>
-#import "Util.h"
+
 @interface VPNWrapper : NSObject
 
-@property (nonatomic) dispatch_queue_t queue;
-extern char* xxxxxxx;
-+ (VPNWrapper*) sharedInstance;
+@property (nonatomic, strong) dispatch_queue_t queue;
+@property (nonatomic, strong) NSArray *options;
 
-- (void) startWithOptions:(NSArray*)options;
++ (VPNWrapper*)sharedInstance;
+
+- (void)startWithOptions:(NSArray*)options;
+
+@end
+
+@implementation VPNWrapper
+
+static VPNWrapper *sharedInstance;
+
++ (VPNWrapper*)sharedInstance {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[VPNWrapper alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (void)startWithOptions:(NSArray*)options {
+    self.options = options;
+    self.queue = dispatch_queue_create("com.example.vpnwrapper", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(self.queue, ^{
+        // Perform VPN setup and start
+        [self configureVPNWith:self.options];
+        [self startVPN];
+    });
+}
+
+- (void)configureVPNWith:(NSArray*)options {
+    // Set up VPN configuration based on options
+}
+
+- (void)startVPN {
+    // Start VPN connection
+}
 
 @end
