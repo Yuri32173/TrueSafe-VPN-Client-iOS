22e01c799b367f59fce5402f2f289e3e55d4a073
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-04-09 20:53:12 +0400

    Update GetHTTPS.cpp

--- build/src/openssl-1.0.1e/MacOS/GetHTTPS.src/GetHTTPS.cpp
+++ build/src/openssl-1.0.1e/MacOS/GetHTTPS.src/GetHTTPS.cpp
@@ -1,205 +1,100 @@
-/*
- *	An demo illustrating how to retrieve a URI from a secure HTTP server.
- *
- *	Author: 	Roy Wood
- *	Date:		September 7, 1999
- *	Comments:	This relies heavily on my MacSockets library.
- *				This project is also set up so that it expects the OpenSSL source folder (0.9.4 as I write this)
- *				to live in a folder called "OpenSSL-0.9.4" in this project's parent folder.  For example:
- *
- *					Macintosh HD:
- *						Development:
- *							OpenSSL-0.9.4:
- *								(OpenSSL sources here)
- *							OpenSSL Example:
- *								(OpenSSL example junk here)
- *
- *
- *				Also-- before attempting to compile this, make sure the aliases in "OpenSSL-0.9.4:include:openssl" 
- *				are installed!  Use the AppleScript applet in the "openssl-0.9.4" folder to do this!
- */
-/* modified to seed the PRNG */
-/* modified to use CRandomizer for seeding */
-
-
-//	Include some funky libs I've developed over time
-
-#include "CPStringUtils.hpp"
-#include "ErrorHandling.hpp"
-#include "MacSocket.h"
-#include "Randomizer.h"
-
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-#include <timer.h>
+#define kHTTPS_DNS "www.apache.org"
+#define kHTTPS_Port 443
+#define kHTTPS_URI "/"
 
-#define kHTTPS_DNS			"www.apache-ssl.org"
-#define kHTTPS_Port			443
-#define kHTTPS_URI			"/"
-#define kMaxStringLength	256
-
-
-//	Forward-declare this
-
-OSErr MyMacSocket_IdleWaitCallback(void *inUserRefPtr);
-
-//	My idle-wait callback.  Doesn't do much, does it?  Silly cooperative multitasking.
-
-OSErr MyMacSocket_IdleWaitCallback(void *inUserRefPtr)
-{
-#pragma unused(inUserRefPtr)
-
-EventRecord		theEvent;
-	::EventAvail(everyEvent,&theEvent);
-	
-	CRandomizer *randomizer = (CRandomizer*)inUserRefPtr;
-	if (randomizer)
-		randomizer->PeriodicAction();
-
-	return(noErr);
+void printError(const char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
 }
 
+void printSSLError(const char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, ERR_error_string(ERR_get_error(), NULL));
+}
 
-//	Finally!
+int main() {
+    SSL_CTX *ctx = NULL;
+    SSL *ssl = NULL;
+    int sockfd = -1, n;
+    char request[512];
+    char response[4096];
 
-void main(void)
-{
-	OSErr				errCode;
-	int					theSocket = -1;
-	int					theTimeout = 30;
+    SSL_library_init();
+    ERR_load_crypto_strings();
+    ERR_load_SSL_strings();
 
-	SSL_CTX				*ssl_ctx = nil;
-	SSL					*ssl = nil;
+    ctx = SSL_CTX_new(TLS_client_method());
+    if (ctx == NULL) {
+        printSSLError("SSL_CTX_new");
+        goto EXIT;
+    }
 
-	char				tempString[256];
-	UnsignedWide		microTickCount;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printError("socket");
+        goto EXIT;
+    }
 
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(kHTTPS_Port);
+    if (inet_pton(AF_INET, kHTTPS_DNS, &serveraddr.sin_addr) <= 0) {
+        printError("inet_pton");
+        goto EXIT;
+    }
 
-	CRandomizer randomizer;
-	
-	printf("OpenSSL Demo by Roy Wood, roy@centricsystems.ca\n\n");
-	
-	BailIfError(errCode = MacSocket_Startup());
+    if (connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
+        printError("connect");
+        goto EXIT;
+    }
 
+    ssl = SSL_new(ctx);
+    if (ssl == NULL) {
+        printSSLError("SSL_new");
+        goto EXIT;
+    }
 
+    SSL_set_fd(ssl, sockfd);
 
-	//	Create a socket-like object
-	
-	BailIfError(errCode = MacSocket_socket(&theSocket,false,theTimeout * 60,MyMacSocket_IdleWaitCallback,&randomizer));
+    if (SSL_connect(ssl) <= 0) {
+        printSSLError("SSL_connect");
+        goto EXIT;
+    }
 
-	
-	//	Set up the connect string and try to connect
-	
-	CopyCStrAndInsertCStrLongIntIntoCStr("%s:%ld",kHTTPS_DNS,kHTTPS_Port,tempString,sizeof(tempString));
-	
-	printf("Connecting to %s....\n",tempString);
+    snprintf(request, sizeof(request), "GET %s HTTP/1.0\r\n\r\n", kHTTPS_URI);
+    if (SSL_write(ssl, request, strlen(request)) < 0) {
+        printSSLError("SSL_write");
+        goto EXIT;
+    }
 
-	BailIfError(errCode = MacSocket_connect(theSocket,tempString));
-	
-	
-	//	Init SSL stuff
-	
-	SSL_load_error_strings();
-	
-	SSLeay_add_ssl_algorithms();
-	
-	
-	//	Pick the SSL method
-	
-//	ssl_ctx = SSL_CTX_new(SSLv2_client_method());
-	ssl_ctx = SSL_CTX_new(SSLv23_client_method());
-//	ssl_ctx = SSL_CTX_new(SSLv3_client_method());
-			
+    while ((n = SSL_read(ssl, response, sizeof(response)-1)) > 0) {
+        response[n] = '\0';
+        printf("%s", response);
+    }
 
-	//	Create an SSL thingey and try to negotiate the connection
-	
-	ssl = SSL_new(ssl_ctx);
-	
-	SSL_set_fd(ssl,theSocket);
-	
-	errCode = SSL_connect(ssl);
-	
-	if (errCode < 0)
-	{
-		SetErrorMessageAndLongIntAndBail("OpenSSL: Can't initiate SSL connection, SSL_connect() = ",errCode);
-	}
-	
-	//	Request the URI from the host
-	
-	CopyCStrToCStr("GET ",tempString,sizeof(tempString));
-	ConcatCStrToCStr(kHTTPS_URI,tempString,sizeof(tempString));
-	ConcatCStrToCStr(" HTTP/1.0\r\n\r\n",tempString,sizeof(tempString));
+    if (n < 0) {
+        printSSLError("SSL_read");
+        goto EXIT;
+    }
 
-	
-	errCode = SSL_write(ssl,tempString,CStrLength(tempString));
-	
-	if (errCode < 0)
-	{
-		SetErrorMessageAndLongIntAndBail("OpenSSL: Error writing data via ssl, SSL_write() = ",errCode);
-	}
-	
+EXIT:
+    if (ssl != NULL) {
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+    }
+    if (ctx != NULL) {
+        SSL_CTX_free(ctx);
+    }
+    if (sockfd >= 0) {
+        close(sockfd);
+    }
 
-	for (;;)
-	{
-	char	tempString[256];
-	int		bytesRead;
-		
-
-		//	Read some bytes and dump them to the console
-		
-		bytesRead = SSL_read(ssl,tempString,sizeof(tempString) - 1);
-		
-		if (bytesRead == 0 && MacSocket_RemoteEndIsClosing(theSocket))
-		{
-			break;
-		}
-		
-		else if (bytesRead < 0)
-		{
-			SetErrorMessageAndLongIntAndBail("OpenSSL: Error reading data via ssl, SSL_read() = ",bytesRead);
-		}
-		
-		
-		tempString[bytesRead] = '\0';
-		
-		printf("%s", tempString);
-	}
-	
-	printf("\n\n\n");
-	
-	//	All done!
-	
-	errCode = noErr;
-	
-	
-EXITPOINT:
-
-	//	Clean up and go home
-	
-	if (theSocket >= 0)
-	{
-		MacSocket_close(theSocket);
-	}
-	
-	if (ssl != nil)
-	{
-		SSL_free(ssl);
-	}
-	
-	if (ssl_ctx != nil)
-	{
-		SSL_CTX_free(ssl_ctx);
-	}
-	
-	
-	if (errCode != noErr)
-	{
-		printf("An error occurred:\n");
-		
-		printf("%s",GetErrorMessage());
-	}
-	
-	
-	MacSocket_Shutdown();
+    return 0;
 }
