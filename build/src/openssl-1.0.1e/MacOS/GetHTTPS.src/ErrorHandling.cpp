71fab9f110ce89a09aabe375932f583174b2821a
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-04-09 20:54:47 +0400

    Update ErrorHandling.cpp

--- build/src/openssl-1.0.1e/MacOS/GetHTTPS.src/ErrorHandling.cpp
+++ build/src/openssl-1.0.1e/MacOS/GetHTTPS.src/ErrorHandling.cpp
@@ -1,170 +1,96 @@
-/* ====================================================================
- * Copyright (c) 1998-1999 The OpenSSL Project.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. All advertising materials mentioning features or use of this
- *    software must display the following acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit. (http://www.openssl.org/)"
- *
- * 4. The names "OpenSSL Toolkit" and "OpenSSL Project" must not be used to
- *    endorse or promote products derived from this software without
- *    prior written permission. For written permission, please contact
- *    openssl-core@openssl.org.
- *
- * 5. Products derived from this software may not be called "OpenSSL"
- *    nor may "OpenSSL" appear in their names without prior written
- *    permission of the OpenSSL Project.
- *
- * 6. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit (http://www.openssl.org/)"
- *
- * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This product includes cryptographic software written by Eric Young
- * (eay@cryptsoft.com).  This product includes software written by Tim
- * Hudson (tjh@cryptsoft.com).
- *
- */
- 
- 
- 
- #include "ErrorHandling.hpp"
+#include "ErrorHandling.hpp"
 #include "CPStringUtils.hpp"
 
-#ifdef __EXCEPTIONS_ENABLED__
-	#include "CMyException.hpp"
-#endif
+static constexpr int kErrorMessageBufferSize = 512;
+static char gErrorMessageBuffer[kErrorMessageBufferSize];
 
+char* const gErrorMessage = gErrorMessageBuffer;
+const int gErrorMessageMaxLength = kErrorMessageBufferSize;
 
-static char					gErrorMessageBuffer[512];
-
-char 						*gErrorMessage = gErrorMessageBuffer;
-int							gErrorMessageMaxLength = sizeof(gErrorMessageBuffer);
-
-
-
-void SetErrorMessage(const char *theErrorMessage)
-{
-	if (theErrorMessage != nil)
-	{
-		CopyCStrToCStr(theErrorMessage,gErrorMessage,gErrorMessageMaxLength);
-	}
+void SetErrorMessage(const char* const theErrorMessage) {
+    if (theErrorMessage != nullptr) {
+        strncpy(gErrorMessage, theErrorMessage, gErrorMessageMaxLength);
+    }
 }
 
-
-void SetErrorMessageAndAppendLongInt(const char *theErrorMessage,const long theLongInt)
-{
-	if (theErrorMessage != nil)
-	{
-		CopyCStrAndConcatLongIntToCStr(theErrorMessage,theLongInt,gErrorMessage,gErrorMessageMaxLength);
-	}
+void SetErrorMessageAndAppendLongInt(const char* const theErrorMessage, const long theLongInt) {
+    if (theErrorMessage != nullptr) {
+        snprintf(gErrorMessage, gErrorMessageMaxLength, "%s%ld", theErrorMessage, theLongInt);
+    }
 }
 
-void SetErrorMessageAndCStrAndLongInt(const char *theErrorMessage,const char * theCStr,const long theLongInt)
-{
-	if (theErrorMessage != nil)
-	{
-		CopyCStrAndInsertCStrLongIntIntoCStr(theErrorMessage,theCStr,theLongInt,gErrorMessage,gErrorMessageMaxLength);
-	}
-
+void SetErrorMessageAndCStrAndLongInt(const char* const theErrorMessage, const char* const theCStr, const long theLongInt) {
+    if (theErrorMessage != nullptr && theCStr != nullptr) {
+        snprintf(gErrorMessage, gErrorMessageMaxLength, "%s%s%ld", theErrorMessage, theCStr, theLongInt);
+    }
 }
 
-void SetErrorMessageAndCStr(const char *theErrorMessage,const char * theCStr)
-{
-	if (theErrorMessage != nil)
-	{
-		CopyCStrAndInsertCStrLongIntIntoCStr(theErrorMessage,theCStr,-1,gErrorMessage,gErrorMessageMaxLength);
-	}
+void SetErrorMessageAndCStr(const char* const theErrorMessage, const char* const theCStr) {
+    if (theErrorMessage != nullptr && theCStr != nullptr) {
+        snprintf(gErrorMessage, gErrorMessageMaxLength, "%s%s", theErrorMessage, theCStr);
+    }
 }
 
-
-void AppendCStrToErrorMessage(const char *theErrorMessage)
-{
-	if (theErrorMessage != nil)
-	{
-		ConcatCStrToCStr(theErrorMessage,gErrorMessage,gErrorMessageMaxLength);
-	}
+void AppendCStrToErrorMessage(const char* const theErrorMessage) {
+    if (theErrorMessage != nullptr) {
+        strncat(gErrorMessage, theErrorMessage, gErrorMessageMaxLength - strlen(gErrorMessage) - 1);
+    }
 }
 
-
-void AppendLongIntToErrorMessage(const long theLongInt)
-{
-	ConcatLongIntToCStr(theLongInt,gErrorMessage,gErrorMessageMaxLength);
+void AppendLongIntToErrorMessage(const long theLongInt) {
+    char longIntBuffer[32];
+    snprintf(longIntBuffer, sizeof(longIntBuffer), "%ld", theLongInt);
+    strncat(gErrorMessage, longIntBuffer, gErrorMessageMaxLength - strlen(gErrorMessage) - 1);
 }
 
-
-
-char *GetErrorMessage(void)
-{
-	return gErrorMessage;
+char* const GetErrorMessage() {
+    return gErrorMessage;
 }
 
-
-OSErr GetErrorMessageInNewHandle(Handle *inoutHandle)
-{
-OSErr		errCode;
-
-
-	errCode = CopyCStrToNewHandle(gErrorMessage,inoutHandle);
-	
-	return(errCode);
+OSErr GetErrorMessageInNewHandle(Handle* const inoutHandle) {
+    if (inoutHandle == nullptr) {
+        return -1;
+    }
+    *inoutHandle = NewHandle(strlen(gErrorMessage) + 1);
+    if (*inoutHandle == nullptr) {
+        return MemError();
+    }
+    strncpy(*inoutHandle, gErrorMessage, GetHandleSize(*inoutHandle));
+    return noErr;
 }
 
-
-OSErr GetErrorMessageInExistingHandle(Handle inoutHandle)
-{
-OSErr		errCode;
-
-
-	errCode = CopyCStrToExistingHandle(gErrorMessage,inoutHandle);
-	
-	return(errCode);
+OSErr GetErrorMessageInExistingHandle(Handle inoutHandle) {
+    if (inoutHandle == nullptr) {
+        return -1;
+    }
+    if (GetHandleSize(inoutHandle) < strlen(gErrorMessage) + 1) {
+        SetHandleSize(inoutHandle, strlen(gErrorMessage) + 1);
+        if (MemError() != noErr) {
+            return MemError();
+        }
+    }
+    strncpy(*inoutHandle, gErrorMessage, GetHandleSize(inoutHandle));
+    return noErr;
 }
 
-
-
-OSErr AppendErrorMessageToHandle(Handle inoutHandle)
-{
-OSErr		errCode;
-
-
-	errCode = AppendCStrToHandle(gErrorMessage,inoutHandle,nil);
-	
-	return(errCode);
+OSErr AppendErrorMessageToHandle(Handle inoutHandle) {
+    if (inoutHandle == nullptr) {
+        return -1;
+    }
+    const size_t currentSize = GetHandleSize(inoutHandle);
+    const size_t errorMessageSize = strlen(gErrorMessage) + 1;
+    SetHandleSize(inoutHandle, currentSize + errorMessageSize);
+    if (MemError() != noErr) {
+        return MemError();
+    }
+    strncat(*inoutHandle, gErrorMessage, errorMessageSize);
+    return noErr;
 }
-
 
 #ifdef __EXCEPTIONS_ENABLED__
 
-void ThrowErrorMessageException(void)
-{
-	ThrowDescriptiveException(gErrorMessage);
+void ThrowErrorMessageException() {
+    throw CMyException(gErrorMessage);
 }
 
 #endif
