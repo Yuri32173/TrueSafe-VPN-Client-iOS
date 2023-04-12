4cf075db6701a48dc5bada9b374c7b79d008105a
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-04-12 22:52:02 +0400

    Update buffer.h

--- build/src/openssl-1.0.2j/openssl/buffer.h
+++ build/src/openssl-1.0.2j/openssl/buffer.h
@@ -1,75 +1,11 @@
-/* crypto/buffer/buffer.h */
-/* Copyright (C) 1995-1998 Eric Young (eay@cryptsoft.com)
- * All rights reserved.
- *
- * This package is an SSL implementation written
- * by Eric Young (eay@cryptsoft.com).
- * The implementation was written so as to conform with Netscapes SSL.
- *
- * This library is free for commercial and non-commercial use as long as
- * the following conditions are aheared to.  The following conditions
- * apply to all code found in this distribution, be it the RC4, RSA,
- * lhash, DES, etc., code; not just the SSL code.  The SSL documentation
- * included with this distribution is covered by the same copyright terms
- * except that the holder is Tim Hudson (tjh@cryptsoft.com).
- *
- * Copyright remains Eric Young's, and as such any Copyright notices in
- * the code are not to be removed.
- * If this package is used in a product, Eric Young should be given attribution
- * as the author of the parts of the library used.
- * This can be in the form of a textual message at program startup or
- * in documentation (online or textual) provided with the package.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *    "This product includes cryptographic software written by
- *     Eric Young (eay@cryptsoft.com)"
- *    The word 'cryptographic' can be left out if the rouines from the library
- *    being used are not cryptographic related :-).
- * 4. If you include any Windows specific code (or a derivative thereof) from
- *    the apps directory (application code) you must include an acknowledgement:
- *    "This product includes software written by Tim Hudson (tjh@cryptsoft.com)"
- *
- * THIS SOFTWARE IS PROVIDED BY ERIC YOUNG ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * The licence and distribution terms for any publically available version or
- * derivative of this code cannot be changed.  i.e. this code cannot simply be
- * copied and put under another distribution licence
- * [including the GNU Public Licence.]
- */
-
 #ifndef HEADER_BUFFER_H
-# define HEADER_BUFFER_H
+#define HEADER_BUFFER_H
 
-# include <openssl/ossl_typ.h>
+#include <openssl/ossl_typ.h>
 
-#ifdef  __cplusplus
-extern "C" {
-#endif
-
-# include <stddef.h>
-
-# if !defined(NO_SYS_TYPES_H)
-#  include <sys/types.h>
-# endif
+#include <cstddef>
+#include <memory>
+#include <string>
 
 /* Already declared in ossl_typ.h */
 /* typedef struct buf_mem_st BUF_MEM; */
@@ -80,20 +16,76 @@ struct buf_mem_st {
     size_t max;                 /* size of buffer */
 };
 
-BUF_MEM *BUF_MEM_new(void);
-void BUF_MEM_free(BUF_MEM *a);
-int BUF_MEM_grow(BUF_MEM *str, size_t len);
-int BUF_MEM_grow_clean(BUF_MEM *str, size_t len);
+class BUF_MEM {
+public:
+    BUF_MEM() : buf_{nullptr}, length_{0}, max_{0} {}
+
+    BUF_MEM(const BUF_MEM &) = delete;
+    BUF_MEM(BUF_MEM &&) = delete;
+    BUF_MEM &operator=(const BUF_MEM &) = delete;
+    BUF_MEM &operator=(BUF_MEM &&) = delete;
+
+    ~BUF_MEM() {
+        if (buf_ != nullptr) {
+            ::OPENSSL_free(buf_);
+            buf_ = nullptr;
+        }
+    }
+
+    char *data() const {
+        return buf_;
+    }
+
+    size_t length() const {
+        return length_;
+    }
+
+    size_t max() const {
+        return max_;
+    }
+
+    bool grow(size_t len) {
+        if (len <= max_) {
+            length_ = len;
+            return true;
+        }
+
+        char *new_buf = static_cast<char *>(::OPENSSL_realloc(buf_, len));
+        if (new_buf == nullptr) {
+            return false;
+        }
+
+        buf_ = new_buf;
+        max_ = len;
+        length_ = len;
+        return true;
+    }
+
+    bool grow_clean(size_t len) {
+        if (!grow(len)) {
+            return false;
+        }
+
+        ::memset(buf_, 0, length_);
+        return true;
+    }
+
+private:
+    char *buf_;
+    size_t length_;
+    size_t max_;
+};
+
 size_t BUF_strnlen(const char *str, size_t maxlen);
-char *BUF_strdup(const char *str);
+std::unique_ptr<char[]> BUF_strdup(const char *str);
 
 /*
  * Like strndup, but in addition, explicitly guarantees to never read past the
  * first |siz| bytes of |str|.
  */
-char *BUF_strndup(const char *str, size_t siz);
+std::unique_ptr<char[]> BUF_strndup(const char *str, size_t siz);
 
-void *BUF_memdup(const void *data, size_t siz);
+std::unique_ptr<void, decltype(&::OPENSSL_free)> BUF_memdup(const void *data, size_t siz);
 void BUF_reverse(unsigned char *out, const unsigned char *in, size_t siz);
 
 /* safe string functions */
@@ -119,7 +111,4 @@ void ERR_load_BUF_strings(void);
 
 /* Reason codes. */
 
-#ifdef  __cplusplus
-}
-#endif
-#endif
+#endif  // HEADER_BUFFER_H
