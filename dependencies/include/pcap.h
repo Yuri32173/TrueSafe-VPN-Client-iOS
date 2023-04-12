f3985e7606e7bde17734f1b0e67e761751363123
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-04-12 22:57:18 +0400

    Update pcap.h

--- dependencies/include/pcap.h
+++ dependencies/include/pcap.h
@@ -33,13 +33,5 @@
  * @(#) $Header: /tcpdump/master/libpcap/pcap.h,v 1.59 2006-10-04 18:09:22 guy Exp $ (LBL)
  */
 
-/*
- * For backwards compatibility.
- *
- * Note to OS vendors: do NOT get rid of this file!  Many applications
- * expect to be able to include <pcap.h>, and at least some of them
- * go through contortions in their configure scripts to try to detect
- * OSes that have "helpfully" moved pcap.h to <pcap/pcap.h> without
- * leaving behind a <pcap.h> file.
- */
+
 #include <pcap/pcap.h>
