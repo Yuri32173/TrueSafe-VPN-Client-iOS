26cc7a36dcc9e5b66f628f01789abcd8790543ef
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-04-27 19:04:29 +0400

    Update dtlstest.c

--- build/src/openssl-1.0.2j/ssl/dtlstest.c
+++ build/src/openssl-1.0.2j/ssl/dtlstest.c
@@ -1,12 +1,3 @@
-/*
- * Copyright 2016 The OpenSSL Project Authors. All Rights Reserved.
- *
- * Licensed under the OpenSSL license (the "License").  You may not use
- * this file except in compliance with the License.  You can obtain a copy
- * in the file LICENSE in the source distribution or at
- * https://www.openssl.org/source/license.html
- */
-
 #include <openssl/bio.h>
 #include <openssl/crypto.h>
 #include <openssl/ssl.h>
@@ -15,36 +6,36 @@
 #include "ssltestlib.h"
 #include "testutil.h"
 
-static char *cert = NULL;
-static char *privkey = NULL;
+static char *cert = nullptr;
+static char *privkey = nullptr;
 
-#define NUM_TESTS   2
-
-
-#define DUMMY_CERT_STATUS_LEN  12
+constexpr int NUM_TESTS = 2;
+constexpr int DUMMY_CERT_STATUS_LEN = 12;
+constexpr int RECORD_SEQUENCE = 10;
 
 unsigned char certstatus[] = {
-    SSL3_RT_HANDSHAKE, /* Content type */
-    0xfe, 0xfd, /* Record version */
-    0, 1, /* Epoch */
-    0, 0, 0, 0, 0, 0x0f, /* Record sequence number */
+    SSL3_RT_HANDSHAKE, // Content type
+    0xfe, 0xfd, // Record version
+    0, 1, // Epoch
+    0, 0, 0, 0, 0, 0x0f, // Record sequence number
     0, DTLS1_HM_HEADER_LENGTH + DUMMY_CERT_STATUS_LEN - 2,
-    SSL3_MT_CERTIFICATE_STATUS, /* Cert Status handshake message type */
-    0, 0, DUMMY_CERT_STATUS_LEN, /* Message len */
-    0, 5, /* Message sequence */
-    0, 0, 0, /* Fragment offset */
-    0, 0, DUMMY_CERT_STATUS_LEN - 2, /* Fragment len */
+    SSL3_MT_CERTIFICATE_STATUS, // Cert Status handshake message type
+    0, 0, DUMMY_CERT_STATUS_LEN, // Message len
+    0, 5, // Message sequence
+    0, 0, 0, // Fragment offset
+    0, 0, DUMMY_CERT_STATUS_LEN - 2, // Fragment len
     0x80, 0x80, 0x80, 0x80, 0x80,
-    0x80, 0x80, 0x80, 0x80, 0x80 /* Dummy data */
+    0x80, 0x80, 0x80, 0x80, 0x80 // Dummy data
 };
-
-#define RECORD_SEQUENCE 10
 
 static int test_dtls_unprocessed(int testidx)
 {
-    SSL_CTX *sctx = NULL, *cctx = NULL;
-    SSL *serverssl1 = NULL, *clientssl1 = NULL;
-    BIO *c_to_s_fbio, *c_to_s_mempacket;
+    SSL_CTX *sctx = nullptr;
+    SSL_CTX *cctx = nullptr;
+    SSL *serverssl1 = nullptr;
+    SSL *clientssl1 = nullptr;
+    BIO *c_to_s_fbio = nullptr;
+    BIO *c_to_s_mempacket = nullptr;
     int testresult = 0;
 
     printf("Starting Test %d\n", testidx);
@@ -64,14 +55,14 @@ static int test_dtls_unprocessed(int testidx)
     }
 
     c_to_s_fbio = BIO_new(bio_f_tls_dump_filter());
-    if (c_to_s_fbio == NULL) {
+    if (c_to_s_fbio == nullptr) {
         printf("Failed to create filter BIO\n");
         goto end;
     }
 
-    /* BIO is freed by create_ssl_connection on error */
-    if (!create_ssl_objects(sctx, cctx, &serverssl1, &clientssl1, NULL,
-                               c_to_s_fbio)) {
+    // BIO is freed by create_ssl_connection on error
+    if (!create_ssl_objects(sctx, cctx, &serverssl1, &clientssl1, nullptr,
+                            c_to_s_fbio)) {
         printf("Unable to create SSL objects\n");
         ERR_print_errors_fp(stdout);
         goto end;
@@ -79,17 +70,9 @@ static int test_dtls_unprocessed(int testidx)
 
     if (testidx == 1)
         certstatus[RECORD_SEQUENCE] = 0xff;
-
-    /*
-     * Inject a dummy record from the next epoch. In test 0, this should never
-     * get used because the message sequence number is too big. In test 1 we set
-     * the record sequence number to be way off in the future. This should not
-     * have an impact on the record replay protection because the record should
-     * be dropped before it is marked as arrivedg
-     */
-    c_to_s_mempacket = SSL_get_wbio(clientssl1);
+            c_to_s_mempacket = SSL_get_wbio(clientssl1);
     c_to_s_mempacket = BIO_next(c_to_s_mempacket);
-    mempacket_test_inject(c_to_s_mempacket, (char *)certstatus,
+    mempacket_test_inject(c_to_s_mempacket, reinterpret_cast<char*>(certstatus),
                           sizeof(certstatus), 1, INJECT_PACKET_IGNORE_REC_SEQ);
 
     if (!create_ssl_connection(serverssl1, clientssl1)) {
@@ -110,7 +93,7 @@ static int test_dtls_unprocessed(int testidx)
 
 int main(int argc, char *argv[])
 {
-    BIO *err = NULL;
+    BIO *err = nullptr;
     int testresult = 0;
 
     if (argc != 3) {
@@ -134,7 +117,7 @@ int main(int argc, char *argv[])
         testresult = 1;
 
     ERR_free_strings();
-    ERR_remove_thread_state(NULL);
+    ERR_remove_thread_state(nullptr);
     EVP_cleanup();
     CRYPTO_cleanup_all_ex_data();
     CRYPTO_mem_leaks(err);
@@ -145,3 +128,4 @@ int main(int argc, char *argv[])
 
     return testresult;
 }
+
