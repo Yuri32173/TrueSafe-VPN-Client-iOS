473658207337f7794ab5008c8634c99af33ca72d
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-04-27 18:55:14 +0400

    Update openvpn-plugin.h

--- dependencies/include/openvpn-plugin.h
+++ dependencies/include/openvpn-plugin.h
@@ -275,6 +275,50 @@ typedef enum {
   SSLAPI_OPENSSL,
   SSLAPI_POLARSSL
 } ovpnSSLAPI;
+class MyClockTick
+    {
+    public:
+      MyClockTick(openvpn_io::io_context& io_context,
+		  OpenVPNClient* parent_arg,
+		  const unsigned int ms)
+	: timer(io_context),
+	  parent(parent_arg),
+	  period(Time::Duration::milliseconds(ms))
+      {
+      }
+
+      void cancel()
+      {
+	timer.cancel();
+      }
+
+      void detach_from_parent()
+      {
+	parent = nullptr;
+      }
+
+      void schedule()
+      {
+	timer.expires_after(period);
+	timer.async_wait([this](const openvpn_io::error_code& error)
+			 {
+			   if (!parent || error)
+			     return;
+			   try {
+			     parent->clock_tick();
+			   }
+			   catch (...)
+			     {
+			     }
+			   schedule();
+			 });
+      }
+
+    private:
+      AsioTimer timer;
+      OpenVPNClient* parent;
+      const Time::Duration period;
+    };
 
 /**
  * Arguments used to transport variables to the plug-in.
