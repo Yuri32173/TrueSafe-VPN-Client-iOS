2ece27ec7996e3f8334071acd4327ef44067ab00
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-05-29 19:23:04 +0400

    Update clean-depend.pl

--- build/src/openssl-1.0.2k/util/clean-depend.pl
+++ build/src/openssl-1.0.2k/util/clean-depend.pl
@@ -1,57 +1,44 @@
-#!/usr/local/bin/perl -w
-# Clean the dependency list in a makefile of standard includes...
-# Written by Ben Laurie <ben@algroup.co.uk> 19 Jan 1999
 
 use strict;
-
-while(<STDIN>) {
-    print;
-    last if /^# DO NOT DELETE THIS LINE/;
-}
+use warnings;
 
 my %files;
 
-my $thisfile="";
-while(<STDIN>) {
-    my ($dummy, $file,$deps)=/^((.*):)? (.*)$/;
-    my $origfile="";
-    $thisfile=$file if defined $file;
-    next if !defined $deps;
-    $origfile=$thisfile;
-    $origfile=~s/\.o$/.c/;
-    my @deps=split ' ',$deps;
-    @deps=grep(!/^\//,@deps);
-    @deps=grep(!/^\\$/,@deps);
-    @deps=grep(!/^$origfile$/,@deps);
-# pull out the kludged kerberos header (if present).
-    @deps=grep(!/^[.\/]+\/krb5.h/,@deps);
-    push @{$files{$thisfile}},@deps;
+while (my $line = <STDIN>) {
+    print $line;
+    last if $line =~ /^# DO NOT DELETE THIS LINE/;
 }
 
-my $file;
-foreach $file (sort keys %files) {
-    my $len=0;
-    my $dep;
-    my $origfile=$file;
-    $origfile=~s/\.o$/.c/;
-    $file=~s/^\.\///;
-    push @{$files{$file}},$origfile;
-    my $prevdep="";
+my $thisfile = "";
+while (my $line = <STDIN>) {
+    chomp $line;
+    my ($dummy, $file, $deps) = $line =~ /^((.*):)? (.*)$/;
+    $thisfile = $file if defined $file;
+    next unless defined $deps;
 
-    # Remove leading ./ before sorting
-    my @deps = map { $_ =~ s/^\.\///; $_ } @{$files{$file}};
+    my $origfile = $thisfile;
+    $origfile =~ s/\.o$/.c/;
+    my @deps = grep { !/^\// && !/^\\$/ && $_ ne $origfile && !/^[.\/]+\/krb5.h/ } split(' ', $deps);
+    push @{$files{$thisfile}}, @deps;
+}
 
-    foreach $dep (sort @deps) {
-	$dep=~s/^\.\///;
-	next if $prevdep eq $dep; # to exterminate duplicates...
-	$prevdep = $dep;
-	$len=0 if $len+length($dep)+1 >= 80;
-	if($len == 0) {
-	    print "\n$file:";
-	    $len=length($file)+1;
-	}
-	print " $dep";
-	$len+=length($dep)+1;
+foreach my $file (sort keys %files) {
+    my $len = 0;
+    my $prevdep = "";
+
+    # Remove leading "./" before sorting
+    my @deps = map { s/^\.\///; $_ } @{$files{$file}};
+
+    foreach my $dep (sort @deps) {
+        next if $prevdep eq $dep; # to exterminate duplicates...
+        $prevdep = $dep;
+        $len = 0 if $len + length($dep) + 1 >= 80;
+        if ($len == 0) {
+            print "\n$file:";
+            $len = length($file) + 1;
+        }
+        print " $dep";
+        $len += length($dep) + 1;
     }
 }
 
