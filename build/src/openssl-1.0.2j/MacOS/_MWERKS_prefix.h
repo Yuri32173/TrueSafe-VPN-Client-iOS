e4ab96bdb97a1466b533fdb196a734a63e546b98
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-06-06 22:03:38 +0400

    Update _MWERKS_prefix.h

--- build/src/openssl-1.0.2j/MacOS/_MWERKS_prefix.h
+++ build/src/openssl-1.0.2j/MacOS/_MWERKS_prefix.h
@@ -1,9 +1,18 @@
 #include <MacHeaders.h>
+#include <stdbool.h>
+
 #define B_ENDIAN
-#ifdef __POWERPC__
-# pragma longlong on
-#endif
-#if 0
-# define MAC_OS_GUSI_SOURCE
-#endif
 #define MONOLITH
+
+#ifdef __POWERPC__
+#pragma longlong on
+#endif
+
+#if 0
+#define MAC_OS_GUSI_SOURCE
+#endif
+
+int main()
+{
+    return 0;
+}
