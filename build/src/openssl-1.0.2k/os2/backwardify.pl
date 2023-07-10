3928e741ada5771d4b7b2760d3f6a108db0a4774
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-07-10 14:01:54 +0400

    Update backwardify.pl

--- build/src/openssl-1.0.2k/os2/backwardify.pl
+++ build/src/openssl-1.0.2k/os2/backwardify.pl
@@ -1,31 +1,38 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 use strict;
-
-# Use as $0
-# Use as $0 -noname
+use warnings;
 
 my $did_library;
 my $did_description;
 my $do_exports;
 my @imports;
 my $noname = (@ARGV and $ARGV[0] eq '-noname' and shift);
+
 while (<>) {
   unless ($did_library) {
-    s/\b(cryptssl)\b/crypto/ and $did_library = $1 if /^LIBRARY\s+cryptssl\b/;
-    s/\b(open_ssl)\b/ssl/    and $did_library = $1 if /^LIBRARY\s+open_ssl\b/;
+    $did_library = $1 if /^LIBRARY\s+(cryptssl|open_ssl)\b/;
+    s/\b(cryptssl|open_ssl)\b/crypto/ if $did_library;
   }
+
   unless ($did_description) {
-    s&^(DESCRIPTION\s+(['"])).*&${1}\@#www.openssl.org/:#\@forwarder DLL for pre-0.9.7c+ OpenSSL to the new dll naming scheme$2& and $did_description++;
+    $did_description++ if s/^(DESCRIPTION\s+(['"])).*/${1}\@#www.openssl.org/:#\@forwarder DLL for pre-0.9.7c+ OpenSSL to the new dll naming scheme$2/;
   }
-  if ($do_exports) {{
+
+  if ($do_exports) {
     last unless /\S/;
-    warn, last unless /^ \s* ( \w+ ) \s+ \@(\d+)\s*$/x;
-    push @imports, [$1, $2];
-    s/$/ NONAME/ if $noname;
-  }}
+    if (/^ \s* ( \w+ ) \s+ \@(\d+)\s*$/x) {
+      push @imports, [$1, $2];
+      s/$/ NONAME/ if $noname;
+    } else {
+      warn;
+      last;
+    }
+  }
+
   $do_exports++ if not $do_exports and /^EXPORTS/;
-  print $_;
+  print;
 }
+
 print "IMPORTS\n";
 for my $imp (@imports) {
   print "\t$imp->[0]=$did_library.$imp->[1]\n";
