67c03dee24543cf02dd921e2ac080c3963c9dcda
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-07-16 21:14:50 +0400

    Update clean-depend.pl

--- build/src/openssl-1.0.2j/util/clean-depend.pl
+++ build/src/openssl-1.0.2j/util/clean-depend.pl
@@ -4,54 +4,52 @@
 
 use strict;
 
-while(<STDIN>) {
+while (<STDIN>) {
     print;
     last if /^# DO NOT DELETE THIS LINE/;
 }
 
 my %files;
 
-my $thisfile="";
-while(<STDIN>) {
-    my ($dummy, $file,$deps)=/^((.*):)? (.*)$/;
-    my $origfile="";
-    $thisfile=$file if defined $file;
+my $thisfile = "";
+while (<STDIN>) {
+    my ($dummy, $file, $deps) = /^((.*):)? (.*)$/;
+    my $origfile = "";
+    $thisfile = $file if defined $file;
     next if !defined $deps;
-    $origfile=$thisfile;
-    $origfile=~s/\.o$/.c/;
-    my @deps=split ' ',$deps;
-    @deps=grep(!/^\//,@deps);
-    @deps=grep(!/^\\$/,@deps);
-    @deps=grep(!/^$origfile$/,@deps);
-# pull out the kludged kerberos header (if present).
-    @deps=grep(!/^[.\/]+\/krb5.h/,@deps);
-    push @{$files{$thisfile}},@deps;
+    $origfile = $thisfile;
+    $origfile =~ s/\.o$/.c/;
+    my @deps = split ' ', $deps;
+    @deps = grep(!/^\//, @deps);
+    @deps = grep(!/^\\$/, @deps);
+    @deps = grep(!/^$origfile$/, @deps);
+    # Pull out the kludged kerberos header (if present).
+    @deps = grep(!/^[.\/]+\/krb5.h/, @deps);
+    push @{$files{$thisfile}}, @deps;
 }
 
-my $file;
-foreach $file (sort keys %files) {
-    my $len=0;
-    my $dep;
-    my $origfile=$file;
-    $origfile=~s/\.o$/.c/;
-    $file=~s/^\.\///;
-    push @{$files{$file}},$origfile;
-    my $prevdep="";
+foreach my $file (sort keys %files) {
+    my $len = 0;
+    my $prevdep = "";
+    my $origfile = $file;
+    $origfile =~ s/\.o$/.c/;
+    $file =~ s/^\.\///;
+    push @{$files{$file}}, $origfile;
 
     # Remove leading ./ before sorting
     my @deps = map { $_ =~ s/^\.\///; $_ } @{$files{$file}};
 
-    foreach $dep (sort @deps) {
-	$dep=~s/^\.\///;
-	next if $prevdep eq $dep; # to exterminate duplicates...
-	$prevdep = $dep;
-	$len=0 if $len+length($dep)+1 >= 80;
-	if($len == 0) {
-	    print "\n$file:";
-	    $len=length($file)+1;
-	}
-	print " $dep";
-	$len+=length($dep)+1;
+    foreach my $dep (sort @deps) {
+        $dep =~ s/^\.\///;
+        next if $prevdep eq $dep; # to exterminate duplicates...
+        $prevdep = $dep;
+        $len = 0 if $len + length($dep) + 1 >= 80;
+        if ($len == 0) {
+            print "\n$file:";
+            $len = length($file) + 1;
+        }
+        print " $dep";
+        $len += length($dep) + 1;
     }
 }
 
