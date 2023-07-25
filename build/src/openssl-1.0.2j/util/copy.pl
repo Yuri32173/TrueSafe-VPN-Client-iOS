6326530346e156b4d56f274e4952594f252ed0fd
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-07-25 14:51:07 +0400

    Update copy.pl

--- build/src/openssl-1.0.2j/util/copy.pl
+++ build/src/openssl-1.0.2j/util/copy.pl
@@ -1,70 +1,58 @@
 #!/usr/local/bin/perl
 
+use strict;
+use warnings;
 use Fcntl;
-
 
 # copy.pl
 
-# Perl script 'copy' comment. On Windows the built in "copy" command also
+# Perl script 'copy' comment. On Windows the built-in "copy" command also
 # copies timestamps: this messes up Makefile dependencies.
 
 my $stripcr = 0;
 
-my $arg;
-
-foreach $arg (@ARGV) {
-	if ($arg eq "-stripcr")
-		{
-		$stripcr = 1;
-		next;
-		}
-	$arg =~ s|\\|/|g;	# compensate for bug/feature in cygwin glob...
-	foreach (glob $arg)
-		{
-		push @filelist, $_;
-		}
+foreach my $arg (@ARGV) {
+    if ($arg eq "-stripcr") {
+        $stripcr = 1;
+        next;
+    }
+    $arg =~ s|\\|/|g;    # compensate for bug/feature in cygwin glob...
+    foreach (glob $arg) {
+        push @filelist, $_;
+    }
 }
 
-$fnum = @filelist;
+my $fnum = @filelist;
 
-if ($fnum <= 1)
-	{
-	die "Need at least two filenames";
-	}
+if ($fnum <= 1) {
+    die "Need at least two filenames";
+}
 
-$dest = pop @filelist;
-	
-if ($fnum > 2 && ! -d $dest)
-	{
-	die "Destination must be a directory";
-	}
+my $dest = pop @filelist;
 
-foreach (@filelist)
-	{
-	if (-d $dest)
-		{
-		$dfile = $_;
-		$dfile =~ s|^.*[/\\]([^/\\]*)$|$1|;
-		$dfile = "$dest/$dfile";
-		}
-	else
-		{
-		$dfile = $dest;
-		}
-	sysopen(IN, $_, O_RDONLY|O_BINARY) || die "Can't Open $_";
-	sysopen(OUT, $dfile, O_WRONLY|O_CREAT|O_TRUNC|O_BINARY)
-					|| die "Can't Open $dfile";
-	while (sysread IN, $buf, 10240)
-		{
-		if ($stripcr)
-			{
-			$buf =~ tr/\015//d;
-			}
-		syswrite(OUT, $buf, length($buf));
-		}
-	close(IN);
-	close(OUT);
-	print "Copying: $_ to $dfile\n";
-	}
-		
+if ($fnum > 2 && ! -d $dest) {
+    die "Destination must be a directory";
+}
 
+foreach my $file (@filelist) {
+    my $dfile;
+    if (-d $dest) {
+        $dfile = $file;
+        $dfile =~ s|^.*[/\\]([^/\\]*)$|$1|;
+        $dfile = "$dest/$dfile";
+    } else {
+        $dfile = $dest;
+    }
+    sysopen(my $IN, $file, O_RDONLY|O_BINARY) || die "Can't Open $file";
+    sysopen(my $OUT, $dfile, O_WRONLY|O_CREAT|O_TRUNC|O_BINARY)
+        || die "Can't Open $dfile";
+    while (sysread $IN, my $buf, 10240) {
+        if ($stripcr) {
+            $buf =~ tr/\015//d;
+        }
+        syswrite($OUT, $buf, length($buf));
+    }
+    close($IN);
+    close($OUT);
+    print "Copying: $file to $dfile\n";
+}
