9a3a295d6c3c4f7172a093b5ab2233231e673233
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-07-25 14:49:48 +0400

    Update b64.pl

--- build/src/openssl-1.0.2j/demos/b64.pl
+++ build/src/openssl-1.0.2j/demos/b64.pl
@@ -4,17 +4,27 @@
 # Make PEM encoded data have lines of 64 bytes of data
 #
 
-while (<>)
-	{
-	if (/^-----BEGIN/ .. /^-----END/)
-		{
-		if (/^-----BEGIN/) { $first=$_; next; }
-		if (/^-----END/) { $last=$_; next; }
-		$out.=$_;
-		}
-	}
+use strict;
+use warnings;
+
+my $out = '';
+my $first;
+my $last;
+
+while (<>) {
+    if (/^-----BEGIN/ .. /^-----END/) {
+        if (/^-----BEGIN/) {
+            $first = $_;
+            next;
+        }
+        if (/^-----END/) {
+            $last = $_;
+            next;
+        }
+        $out .= $_;
+    }
+}
+
 $out =~ s/\s//g;
 $out =~ s/(.{64})/$1\n/g;
 print "$first$out\n$last\n";
-
-
