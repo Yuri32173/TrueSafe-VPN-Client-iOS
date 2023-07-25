f2958491ad9460cabbf1c6f9c9be53186c19f2e1
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-07-25 14:48:37 +0400

    Update add_cr.pl

--- build/src/openssl-1.0.2j/util/add_cr.pl
+++ build/src/openssl-1.0.2j/util/add_cr.pl
@@ -6,58 +6,54 @@
 # perl util/add_cr.pl *.[ch] */*.[ch] */*/*.[ch]
 #
 
-foreach (@ARGV)
-	{
-	&dofile($_);
-	}
+foreach my $file (@ARGV) {
+    dofile($file);
+}
 
-sub dofile
-	{
-	local($file)=@_;
+sub dofile {
+    my ($file) = @_;
 
-	open(IN,"<$file") || die "unable to open $file:$!\n";
+    open(my $IN, "<", $file) || die "unable to open $file: $!\n";
 
-	print STDERR "doing $file\n";
-	@in=<IN>;
+    print STDERR "doing $file\n";
+    my @in = <$IN>;
 
-	return(1) if ($in[0] =~ / NOCW /);
+    return 1 if ($in[0] =~ / NOCW /);
 
-	@out=();
-	open(OUT,">$file.out") || die "unable to open $file.$$:$!\n";
-	push(@out,"/* $file */\n");
-	if (($in[1] !~ /^\/\* Copyright \(C\) [0-9-]+ Eric Young \(eay\@cryptsoft.com\)/))
-		{
-		push(@out,&Copyright);
-		$i=2;
-		@a=grep(/ Copyright \(C\) /,@in);
-		if ($#a >= 0)
-			{
-			while (($i <= $#in) && ($in[$i] ne " */\n"))
-				{ $i++; }
-			$i++ if ($in[$i] eq " */\n");
+    my @out = ();
+    open(my $OUT, ">", "$file.out") || die "unable to open $file.$$: $!\n";
+    push(@out, "/* $file */\n");
+    if (($in[1] !~ /^\/\* Copyright \(C\) [0-9-]+ Eric Young \(eay\@cryptsoft.com\)/)) {
+        push(@out, &Copyright);
+        my $i = 2;
+        my @a = grep(/ Copyright \(C\) /, @in);
+        if ($#a >= 0) {
+            while (($i <= $#in) && ($in[$i] ne " */\n")) {
+                $i++;
+            }
+            $i++ if ($in[$i] eq " */\n");
 
-			while (($i <= $#in) && ($in[$i] =~ /^\s*$/))
-				{ $i++; }
+            while (($i <= $#in) && ($in[$i] =~ /^\s*$/)) {
+                $i++;
+            }
 
-			push(@out,"\n");
-			for ( ; $i <= $#in; $i++)
-				{ push(@out,$in[$i]); }
-			}
-		else
-			{ push(@out,@in); }
-		}
-	else
-		{
-		shift(@in);
-		push(@out,@in);
-		}
-	print OUT @out;
-	close(IN);
-	close(OUT);
-	rename("$file","$file.orig") || die "unable to rename $file:$!\n";
-	rename("$file.out",$file) || die "unable to rename $file.out:$!\n";
-	}
-
+            push(@out, "\n");
+            for (; $i <= $#in; $i++) {
+                push(@out, $in[$i]);
+            }
+        } else {
+            push(@out, @in);
+        }
+    } else {
+        shift(@in);
+        push(@out, @in);
+    }
+    print $OUT @out;
+    close($IN);
+    close($OUT);
+    rename("$file", "$file.orig") || die "unable to rename $file: $!\n";
+    rename("$file.out", $file) || die "unable to rename $file.out: $!\n";
+}
 
 
 sub Copyright
