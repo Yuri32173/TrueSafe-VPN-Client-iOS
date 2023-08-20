72cc97993e5f78b279dff4659c57a2f6fd0d2a25
Yury Kozharsky <128378190+Yuri32173@users.noreply.github.com>
2023-08-20 20:23:26 +0400

    Update extract-names.pl

--- build/src/openssl-1.0.2k/util/extract-names.pl
+++ build/src/openssl-1.0.2k/util/extract-names.pl
@@ -1,26 +1,31 @@
 #!/usr/bin/perl
 
-$/ = "";			# Eat a paragraph at once.
-while(<STDIN>) {
+$/ = "";  # Eat a paragraph at once.
+
+my $name_section = 0;
+my @words;
+
+while (<STDIN>) {
     chop;
     s/\n/ /gm;
+
     if (/^=head1 /) {
-	$name = 0;
-    } elsif ($name) {
-	if (/ - /) {
-	    s/ - .*//;
-	    s/,\s+/,/g;
-	    s/\s+,/,/g;
-	    s/^\s+//g;
-	    s/\s+$//g;
-	    s/\s/_/g;
-	    push @words, split ',';
-	}
+        $name_section = 0;
+    } elsif ($name_section) {
+        if (/ - /) {
+            s/ - .*//;
+            s/,\s+/,/g;
+            s/\s+,/,/g;
+            s/^\s+//g;
+            s/\s+$//g;
+            s/\s/_/g;
+            push @words, split ',';
+        }
     }
+
     if (/^=head1 *NAME *$/) {
-	$name = 1;
+        $name_section = 1;
     }
 }
 
-print join("\n", @words),"\n";
-
+print join("\n", @words), "\n";
